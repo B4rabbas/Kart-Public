b084318a7dbf6dc48f203cc6eb6a89184bdf31cf
Barabbas <36156012+B4rabbas@users.noreply.github.com>
2020-06-12 16:47:59 +0200

    Update i_video.c

--- src/sdl/i_video.c
+++ src/sdl/i_video.c
@@ -267,16 +267,16 @@ static void SDLSetMode(INT32 width, INT32 height, SDL_bool fullscreen)
 
 static INT32 Impl_SDL_Scancode_To_Keycode(SDL_Keycode code)
 {
-	if (code >= SDLK_A && code <= SDLK_Z)
+	if (code >= SDLK_a && code <= SDLK_z)
 	{
 		// get lowercase ASCII
-		return code - SDLK_A + 'a';
+		return code - SDLK_a + 'a';
 	}
 	if (code >= SDLK_1 && code <= SDLK_9)
 	{
 		return code - SDLK_1 + '1';
 	}
-	else if (code == SDLK_SCANCODE_0)
+	else if (code == SDLK_0)
 	{
 		return '0';
 	}
@@ -311,10 +311,10 @@ static INT32 Impl_SDL_Scancode_To_Keycode(SDL_Keycode code)
 		case SDLK_LEFTBRACKET:    return '[';
 		case SDLK_RIGHTBRACKET:   return ']';
 		case SDLK_BACKSLASH:      return '\\';
-		case SDLK_NONUSHASH:      return '#';
+		case SDLK_HASH:           return '#';
 		case SDLK_SEMICOLON:      return ';';
-		case SDLK_APOSTROPHE:     return '\'';
-		case SDLK_GRAVE:          return '`';
+		case SDLK_QUOTE:          return '\'';
+		case SDLK_BACKQUOTE:      return '`';
 		case SDLK_COMMA:          return ',';
 		case SDLK_PERIOD:         return '.';
 		case SDLK_SLASH:          return '/';
@@ -339,7 +339,6 @@ static INT32 Impl_SDL_Scancode_To_Keycode(SDL_Keycode code)
 		case SDLK_KP_PLUS:        return KEY_PLUSPAD;
 		case SDLK_KP_ENTER:       return KEY_ENTER;
 		case SDLK_KP_PERIOD:      return KEY_KPADDEL;
-		case SDLK_NONUSBACKSLASH: return '\\';
 
 		case SDLK_LSHIFT: return KEY_LSHIFT;
 		case SDLK_RSHIFT: return KEY_RSHIFT;
@@ -351,14 +350,12 @@ static INT32 Impl_SDL_Scancode_To_Keycode(SDL_Keycode code)
 		case SDLK_RGUI:   return KEY_RIGHTWIN;
 		default:                  break;
 	}
-	return 0;
-}
 #ifdef HWRENDER
-DBG_Printf("Unknown incoming scancode: %d, represented %c\n",
-code,
-SDL_GetKeyName(SDL_GetKeyFromScancode(code)));
+	DBG_Printf("Unknown incoming scancode: %d, represented %c\n",
+				code,
+				SDL_GetKeyName(SDL_GetKeyFromScancode(code)));
 #endif
-return 0;
+	return 0;
 }
 
 static void SDLdoGrabMouse(void)
@@ -637,6 +634,11 @@ static void Impl_HandleWindowEvent(SDL_WindowEvent evt)
 			if (cv_usemouse.value) I_StartupMouse();
 		}
 		//else firsttimeonmouse = SDL_FALSE;
+
+		capslock = !!( SDL_GetModState() & KMOD_CAPS );// in case CL changes
+
+		if (USE_MOUSEINPUT)
+			SDLdoGrabMouse();
 	}
 	else if (!mousefocus && !kbfocus)
 	{
@@ -1293,8 +1295,6 @@ void I_StartupMouse(void)
 //
 void I_OsPolling(void)
 {
-	SDL_Keymod mod;
-
 	if (consolevent)
 		I_GetConsoleEvents();
 	if (SDL_WasInit(SDL_INIT_JOYSTICK) == SDL_INIT_JOYSTICK)
@@ -1309,18 +1309,6 @@ void I_OsPolling(void)
 	I_GetMouseEvents();
 
 	I_GetEvent();
-
-	mod = SDL_GetModState();
-	/* Handle here so that our state is always synched with the system. */
-	shiftdown = ctrldown = altdown = 0;
-	capslock = false;
-	if (mod & KMOD_LSHIFT) shiftdown |= 1;
-	if (mod & KMOD_RSHIFT) shiftdown |= 2;
-	if (mod & KMOD_LCTRL)   ctrldown |= 1;
-	if (mod & KMOD_RCTRL)   ctrldown |= 2;
-	if (mod & KMOD_LALT)     altdown |= 1;
-	if (mod & KMOD_RALT)     altdown |= 2;
-	if (mod & KMOD_CAPS) capslock = true;
 }
 
 //
@@ -1652,7 +1640,6 @@ INT32 VID_SetMode(INT32 modeNum)
 	//Impl_SetWindowName("SRB2Kart "VERSIONSTRING);
 
 	SDLSetMode(vid.width, vid.height, USE_FULLSCREEN);
-	Impl_VideoSetupBuffer();
 
 	if (rendermode == render_soft)
 	{
@@ -1661,6 +1648,8 @@ INT32 VID_SetMode(INT32 modeNum)
 			SDL_FreeSurface(bufSurface);
 			bufSurface = NULL;
 		}
+
+		Impl_VideoSetupBuffer();
 	}
 
 	return SDL_TRUE;
@@ -1783,7 +1772,7 @@ static void Impl_VideoSetupSDLBuffer(void)
 static void Impl_VideoSetupBuffer(void)
 {
 	// Set up game's software render buffer
-	//if (rendermode == render_soft)
+	if (rendermode == render_soft)
 	{
 		vid.rowbytes = vid.width * vid.bpp;
 		vid.direct = NULL;
@@ -1838,11 +1827,9 @@ void I_StartupGraphics(void)
 			framebuffer = SDL_TRUE;
 	}
 	if (M_CheckParm("-software"))
+	{
 		rendermode = render_soft;
-#ifdef HWRENDER
-	else if (M_CheckParm("-opengl"))
-		rendermode = render_opengl;
-#endif
+	}
 
 	usesdl2soft = M_CheckParm("-softblit");
 	borderlesswindow = M_CheckParm("-borderless");
@@ -1850,8 +1837,9 @@ void I_StartupGraphics(void)
 	//SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY>>1,SDL_DEFAULT_REPEAT_INTERVAL<<2);
 	VID_Command_ModeList_f();
 #ifdef HWRENDER
-	if (rendermode == render_opengl)
+	if (M_CheckParm("-opengl") || rendermode == render_opengl)
 	{
+		rendermode = render_opengl;
 		HWD.pfnInit             = hwSym("Init",NULL);
 		HWD.pfnFinishUpdate     = NULL;
 		HWD.pfnDraw2DLine       = hwSym("Draw2DLine",NULL);
@@ -1868,6 +1856,7 @@ void I_StartupGraphics(void)
 		HWD.pfnDrawModel        = hwSym("DrawModel",NULL);
 		HWD.pfnCreateModelVBOs  = hwSym("CreateModelVBOs",NULL);
 		HWD.pfnSetTransform     = hwSym("SetTransform",NULL);
+		HWD.pfnGetRenderVersion = hwSym("GetRenderVersion",NULL);
 		HWD.pfnPostImgRedraw    = hwSym("PostImgRedraw",NULL);
 		HWD.pfnFlushScreenTextures=hwSym("FlushScreenTextures",NULL);
 		HWD.pfnStartScreenWipe  = hwSym("StartScreenWipe",NULL);
@@ -1877,22 +1866,13 @@ void I_StartupGraphics(void)
 		HWD.pfnMakeScreenTexture= hwSym("MakeScreenTexture",NULL);
 		HWD.pfnMakeScreenFinalTexture=hwSym("MakeScreenFinalTexture",NULL);
 		HWD.pfnDrawScreenFinalTexture=hwSym("DrawScreenFinalTexture",NULL);
-
-		HWD.pfnRenderSkyDome = hwSym("RenderSkyDome",NULL);
-
-		HWD.pfnLoadShaders = hwSym("LoadShaders",NULL);
-		HWD.pfnKillShaders = hwSym("KillShaders",NULL);
-		HWD.pfnSetShader = hwSym("SetShader",NULL);
-		HWD.pfnUnSetShader = hwSym("UnSetShader",NULL);
-
-		HWD.pfnLoadCustomShader = hwSym("LoadCustomShader",NULL);
-		HWD.pfnInitCustomShaders = hwSym("InitCustomShaders",NULL);
-
-		HWD.pfnStartBatching = hwSym("StartBatching",NULL);
-		HWD.pfnRenderBatches = hwSym("RenderBatches",NULL);
-
-		if (!HWD.pfnInit()) // load the OpenGL library
+		// check gl renderer lib
+		if (HWD.pfnGetRenderVersion() != VERSION)
+			I_Error("%s", M_GetText("The version of the renderer doesn't match the version of the executable\nBe sure you have installed SRB2Kart properly.\n"));
+		if (!HWD.pfnInit(I_Error)) // let load the OpenGL library
+		{
 			rendermode = render_soft;
+		}
 	}
 #endif
 
